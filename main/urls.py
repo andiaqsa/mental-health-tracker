293d20be1d55c03427ad3d92a838fc0a5b159198
ANDI AQSA MAPPATUNRU MARZUKI <andiaqsa365@gmail.com>
2024-09-25 08:03:48 +0700

    Tutorial 3 selesai

--- main/urls.py
+++ main/urls.py
@@ -1,5 +1,7 @@
 from django.urls import path
-from main.views import show_main, create_mood_entry, show_xml, show_json, show_xml_by_id, show_json_by_id
+from django.contrib.auth import views as auth_views
+from main.views import show_main, create_mood_entry, show_xml, show_json, show_xml_by_id, show_json_by_id, register
+from main.views import login_user, logout_user
 
 app_name = 'main'
 
@@ -10,4 +12,8 @@ urlpatterns = [
     path('xml/<uuid:id>/', show_xml_by_id, name='show_xml_by_id'),
     path('json/<uuid:id>/', show_json_by_id, name='show_json_by_id'),
     path('create-mood-entry', create_mood_entry, name='create_mood_entry'),
+    path('register/', register, name='register'),
+    path('login/', login_user, name='login'),
+    path('logout/', logout_user, name='logout'),
+
 ]
--- mental_health_tracker/settings.py
+++ mental_health_tracker/settings.py
@@ -9,8 +9,9 @@ https://docs.djangoproject.com/en/5.1/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/5.1/ref/settings/
 """
-
+import os
 from pathlib import Path
+
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -23,7 +24,8 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-h+@p%kf)z*a4zoac30cxxqf(m(f8+!xn-p3a*@bs&11cysnnc8'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+PRODUCTION = os.getenv("PRODUCTION", False)
+DEBUG = not PRODUCTION
 
 ALLOWED_HOSTS = ["localhost", "127.0.0.1", "andi-aqsa-mentalhealthtracker.pbp.cs.ui.ac.id"]
 
@@ -122,3 +124,8 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+LOGIN_URL = 'main:login'  # Arahkan ke halaman login
+LOGIN_REDIRECT_URL = 'main:show_main'  # Setelah login sukses
+LOGOUT_REDIRECT_URL = 'main:login'  # Setelah logout sukses
+
